096a71e7fd545253173f91011e7028a1efb717a9
subramanyamdvss <subramanyamdvss@gmail.com>
2024-08-26 19:11:33 +0000

    added criteo  submission

--- shampoo_criteo_submission.sh
+++ shampoo_criteo_submission.sh
@@ -1,14 +1,13 @@
 
 python scoring/run_workloads.py --framework pytorch \
---experiment_name shampoo_ogbg_model_size_experiment \
+--experiment_name shampoo_criteo_experiment \
 --num_studies 3 \
 --num_tuning_trials 5 \
 --seed 10 \
---hparam_start_index 1 \
---hparam_end_index 2 \
+--hparam_start_index 0 \
+--hparam_end_index 1 \
 --run_percentage 100 \
 --workload_metadata_path "scoring/workload_metadata_external_tuning.json" \
---held_out_workloads_config_path "scoring/held_out_workloads_algoperf_v05.json" \
 --submission_path  "submissions/submissions_algorithms_v0_5/AlgoPerf_Team_21/external_tuning/shampoo_submission/submission.py" \
 --tuning_search_space "submissions/submissions_algorithms_v0_5/AlgoPerf_Team_21/external_tuning/shampoo_submission/tuning_search_space.json" \
---docker_image_url "us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_both_main" --local --workload "ogbg_model_size"
+--docker_image_url "us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_both_main" --local --workload "criteo"

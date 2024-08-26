9cc4419d6ae8ef195cf9ca4e9ce003b44a839762
subramanyamdvss <subramanyamdvss@gmail.com>
2024-08-26 17:27:09 +0000

    added ogbg model size benchmark

---
+++ casprpp_ogbg_model_size_submission.sh
@@ -0,0 +1,14 @@
+
+python scoring/run_workloads.py --framework pytorch \
+--experiment_name casprpp_ogbg_model_size_experiment \
+--num_studies 3 \
+--num_tuning_trials 5 \
+--seed 10 \
+--hparam_start_index 3 \
+--hparam_end_index 4 \
+--run_percentage 100 \
+--workload_metadata_path "scoring/workload_metadata_external_tuning.json" \
+--held_out_workloads_config_path "scoring/held_out_workloads_algoperf_v05.json" \
+--submission_path  "submissions/submissions_algorithms_v0_5/AlgoPerf_Team_21/external_tuning/shampoo_submission/submission_casprpp.py" \
+--tuning_search_space "submissions/submissions_algorithms_v0_5/AlgoPerf_Team_21/external_tuning/shampoo_submission/tuning_search_space.json" \
+--docker_image_url "us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_both_main" --local --workload "ogbg_model_size"
